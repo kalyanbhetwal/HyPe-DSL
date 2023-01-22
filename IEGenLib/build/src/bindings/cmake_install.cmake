a65676241054d3c84efe8ce642175fa9a86612c7
kalyanbhetwal <kalyanbtl@gmail.com>
2023-01-22 12:28:32 -0700

    working base version of IEGenlib python bindings

--- IEGenLib/build/src/bindings/cmake_install.cmake
+++ IEGenLib/build/src/bindings/cmake_install.cmake
@@ -37,3 +37,38 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bindings/_iegenlib.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bindings/_iegenlib.so")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bindings/_iegenlib.so"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings" TYPE SHARED_LIBRARY FILES "/home/KALYANBHETWAL/HyPe-DSL/IEGenLib/build/src/bindings/_iegenlib.so")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bindings/_iegenlib.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bindings/_iegenlib.so")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bindings/_iegenlib.so"
+         OLD_RPATH "/home/KALYANBHETWAL/HyPe-DSL/IEGenLib/lib/omega/install/lib:/home/KALYANBHETWAL/HyPe-DSL/IEGenLib/lib/installed/lib:"
+         NEW_RPATH "")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bindings/_iegenlib.so")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bindings" TYPE FILE FILES "/home/KALYANBHETWAL/HyPe-DSL/IEGenLib/build/src/bindings/iegenlib.py")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  MESSAGE ("
+
+Set IEGEN_HOME to /usr/local
+
+")
+endif()
+
